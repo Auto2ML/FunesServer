8ca08d8cddb5a4349fb94ba3827270d634688bdf
Julio Rodriguez Martino <julio.rodriguezmartino@auto2ml.tech>
2025-03-18 18:18:21 -0300

    Simplify demo launch parameters in funes.py

--- funes.py
+++ funes.py
@@ -2,10 +2,4 @@ from interface import setup_gradio
 
 if __name__ == "__main__":
     demo = setup_gradio()
-    demo.launch(share=False,
-                debug=False,
-                server_name="0.0.0.0",
-                server_port=7680,
-                ssl_verify=False,
-                ssl_certfile="/home/julio/cert.pem",
-                ssl_keyfile="/home/julio/key.pem")
+    demo.launch(server_name="0.0.0.0")
