4882ab5ee3c3a66fc68b5972a6354ea5b413f7b4
Julio Rodriguez Martino <julio.rodriguezmartino@auto2ml.tech>
2025-03-28 16:13:48 -0300

    Update LLM configuration to change backend type to 'llamacpp' and adjust model name

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ Configuration settings for Funes server
 
 # LLM model configuration
 LLM_CONFIG = {
-    'model_name': 'llama3.2:latest',
-    'backend_type': 'ollama',  # Options: 'ollama', 'llamacpp', 'huggingface'
-    #'model_name': '/home/julio/.ollama/models/blobs/sha256-dde5aa3fc5ffc17176b5e8bdc82f587b24b2678c6c66101bf7da77af9f7ccdff',
+    #'model_name': 'llama3.2:latest',
+    'backend_type': 'llamacpp',  # Options: 'ollama', 'llamacpp', 'huggingface'
+    'model_name': '/home/julio/.ollama/models/blobs/sha256-dde5aa3fc5ffc17176b5e8bdc82f587b24b2678c6c66101bf7da77af9f7ccdff',
     'system_prompt': "You are Funes, a helpful assistant. Use your recent conversation history and relevant memories stored in your database to provide more informed and consistent responses, only if it is relevant for the conversation. Do not repeat unnecessary information. If you don't find relevant information in history or memories, use your training data"
 }
 
